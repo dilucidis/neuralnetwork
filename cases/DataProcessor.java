92e65812aa3df60542a8aa1cd1756199be8db40c
rohitr97 <rohit.ramnani@gmail.com>
2016-05-11 15:28:22 -0400

    Add files via upload

---
+++ cases/DataProcessor.java
@@ -0,0 +1,44 @@
+package cases;
+import java.io.*;
+public class DataProcessor {
+	private static File out=new File("out.txt");
+	private static FileWriter fw;
+	private static BufferedWriter bw;
+	
+	private static void write(String move) {
+		try{
+			fw=new FileWriter(out);
+			bw=new BufferedWriter(fw);
+			bw.write(move);
+			bw.newLine();
+			bw.close();
+			fw.close();
+		}catch(IOException e){
+			System.err.println("IOException (Problem writing to file): "+e.getMessage());
+		}
+	}
+
+	public static void main(String[] args) {
+		File in=new File("in.txt");
+		FileReader fr; 
+		BufferedReader br; 
+		String line;
+		Generator gen;
+		
+		try {
+			fr=new FileReader(in);
+			br=new BufferedReader(fr);
+			while((line=br.readLine())!=null){
+				gen=new Generator(line);
+				write(gen.getOut());
+			}
+			br.close();
+			fr.close();
+		}catch(FileNotFoundException e){
+			System.err.println("FileNotFoundException: "+e.getMessage());
+		}catch(IOException e){
+			System.err.println("IOException (Problem reading file): "+e.getMessage());
+		}
+	}
+
+}
---
+++ cases/IOConverter.java
@@ -0,0 +1,62 @@
+package cases;
+
+import io.IO;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+
+public class IOConverter {
+	private File in, out;
+	private FileReader fr1,fr2;
+	private BufferedReader br1,br2;
+	private String inLine,outLine;
+	private IO[] ioList;
+	private boolean[] input,output;
+	private LineNumberReader lnr;
+	
+	public IOConverter(String inLoc, String outLoc) throws IOException {
+		lnr = new LineNumberReader(new FileReader(new File(inLoc)));
+		lnr.skip(Long.MAX_VALUE); //skips to last character before Long.MAX_VALUE characters.
+		in=new File(inLoc);
+		out=new File(outLoc);
+		ioList=new IO[lnr.getLineNumber()+1];
+		
+		try {
+			fr1=new FileReader(in);
+			br1=new BufferedReader(fr1);
+			fr2=new FileReader(out);
+			br2=new BufferedReader(fr2);
+			for(int i=0;(inLine=br1.readLine())!=null||(outLine=br2.readLine())!=null;i++){
+				convert(inLine,outLine,i);
+			}
+			br1.close(); br2.close();
+			fr1.close(); fr2.close();
+		}catch(FileNotFoundException e){
+			System.err.println("FileNotFoundException: "+e.getMessage());
+		}catch(IOException e){
+			System.err.println("IOException (Problem reading file): "+e.getMessage());
+		}
+	}
+
+	private void convert(String inLine2, String outLine2, int index) {
+		input=new boolean[inLine2.length()];
+		output=new boolean[outLine2.length()];
+		setBoolArr(input, inLine2);
+		setBoolArr(output, outLine2);
+		ioList[index]=new IO(input, output);
+	}
+
+	private void setBoolArr(boolean[] arr, String str) {
+		for (int i=0;i<str.length();i++)
+			arr[i]=(Integer.parseInt(""+str.charAt(i))!=0);
+	}
+	
+	public IO[] get(){
+		return ioList;
+	}
+}
