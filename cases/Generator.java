5d4b7bd30fd612bcd40ec3de5f181bc7d556186b
Rohit Ramnani <rohit.ramnani@gmail.com>
2016-05-16 22:50:59 -0400

    Fixed isWin() indices, initialized board[]

--- cases/Generator.java
+++ cases/Generator.java
@@ -6,12 +6,12 @@ import java.awt.Point;
 import java.util.Arrays;
 
 public class Generator {
-	private String out, in;
+	private String out;
 	private Point[] board;
 	private int index, score;
 	
 	public Generator(String in){
-		this.in=in;
+		board=new Point[in.length()/2];
 		for (int i=0;i<in.length()/2;i++){
 			board[i]=new Point(Integer.parseInt(""+in.charAt(2*i)), Integer.parseInt(""+in.charAt(2*i+1))); //pairs booleans
 		}
@@ -30,7 +30,7 @@ public class Generator {
 			isMax=false;
 		}
 		
-		while(a>=b){
+		while(a>=b && a>=alpha && b<=beta){
 			if(isWin()==1) //set score if a player wins
 				if (isMax) a=depth-10;
 				else b=depth-10;
@@ -69,31 +69,31 @@ public class Generator {
 	}
 	
 	private int isWin(){
-		if(board[1].x==1 && board[2].x==1 && board[3].x==1  
-		&& board[1].y==board[2].y && board[2].y==board[3].y 
-		|| board[1].x==1 && board[4].x==1 && board[7].x==1  
-		&& board[1].y==board[4].y && board[4].y==board[7].y){
-			if(board[1].y==1) return 1;
+		if(board[0].x==1 && board[1].x==1 && board[2].x==1  
+		&& board[0].y==board[1].y && board[1].y==board[2].y 
+		|| board[0].x==1 && board[3].x==1 && board[6].x==1  
+		&& board[0].y==board[3].y && board[3].y==board[6].y){
+			if(board[0].y==1) return 1;
 			else return -1;
 		}
 		
-		if(board[4].x==1 && board[5].x==1 && board[6].x==1
-		&& board[4].y==board[5].y && board[5].y==board[6].y
+		if(board[3].x==1 && board[4].x==1 && board[5].x==1
+		&& board[3].y==board[4].y && board[4].y==board[5].y
+		|| board[1].x==1 && board[4].x==1 && board[7].x==1
+		&& board[1].y==board[4].y && board[4].y==board[7].y
+		|| board[0].x==1 && board[4].x==1 && board[8].x==1 
+		&& board[0].y==board[4].y && board[4].y==board[8].y
+		|| board[2].x==1 && board[4].x==1 && board[6].x==1
+		&& board[2].y==board[4].y && board[4].y==board[6].y){
+			if(board[4].y==1) return 1;
+			else return -1;
+		}
+		
+		if(board[6].x==1 && board[7].x==1 && board[8].x==1
+		&& board[6].y==board[7].y && board[7].y==board[8].y
 		|| board[2].x==1 && board[5].x==1 && board[8].x==1
-		&& board[2].y==board[5].y && board[5].y==board[8].y
-		|| board[1].x==1 && board[5].x==1 && board[9].x==1 
-		&& board[1].y==board[5].y && board[5].y==board[9].y
-		|| board[3].x==1 && board[5].x==1 && board[7].x==1
-		&& board[3].y==board[5].y && board[5].y==board[7].y){
-			if(board[5].y==1) return 1;
-			else return -1;
-		}
-		
-		if(board[7].x==1 && board[8].x==1 && board[9].x==1
-		&& board[7].y==board[8].y && board[8].y==board[9].y
-		|| board[3].x==1 && board[6].x==1 && board[9].x==1
-		&& board[3].y==board[6].y && board[6].y==board[9].y){
-			if(board[9].y==1) return 1;
+		&& board[2].y==board[5].y && board[5].y==board[8].y){
+			if(board[8].y==1) return 1;
 			else return -1;
 		}
 		
@@ -101,7 +101,7 @@ public class Generator {
 	}
 
 	public String getOut(){
-		//not done yet
+		out=Integer.toString(index, 2);
 		return out;
 	}
 
