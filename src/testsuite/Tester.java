c641be590172664a277d35c6c62f80fd628eb907
Shane Parr <dilucidis@gmail.com>
2017-05-01 12:00:01 -0400

    overhaul and modernize test suite, failing data and thus network tests

--- src/testsuite/Tester.java
+++ src/testsuite/Tester.java
@@ -1,5 +1,7 @@
 package testsuite;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
 
 import data.Data;
@@ -7,106 +9,104 @@ import io.IO;
 import perceptual_network.*;
 import neurons.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class Tester {
 
 	/**
 	 * @param args
 	 */
 	static String path;
-	private static void setPath(String path){
-		Tester.path = path;
+	
+	@Before
+	public void setUp(){
+		setPath();
+		reset();
 	}
-	private static void testPerceptrons(){
+	public void setPath(){
+		Tester.path = "/Users/sparr/workspace/neuralnetwork/src/data/datasets.txt";
+	}
+	public void reset(){
+		Perceptron.resetNum();
+		Neuron.resetNum();
+		InnerLayer.resetNum();
+		Layer.resetNum();
+		InputNeuron.resetNum();
+	}
+	@Test
+	public void testPerceptrons(){
 		Perceptron p = new Perceptron();
 		Perceptron c = new Perceptron(p);
 		c.setDefaultWeight(1);
 		c.addInput(p);
-		System.out.println("c inputs size: "+c.getInputsAndWeights().size());
-		System.out.println("weight for p in c: "+c.getInputsAndWeights().get(p));
+		assertEquals(c.getInputsAndWeights().size(),1);
+		assertTrue(c.getInputsAndWeights().get(p)==1.0);
 		c.update();
-		System.out.println(c.checkFire());
+		assertTrue(c.checkFire()==0.0);
 		p.manualFire(true);
 		c.update();
-		System.out.println(c.checkFire());
-		System.out.println(Perceptron.getNum()+"+"+InputNeuron.getNum());
-		System.out.println(Neuron.getNum());
-		System.out.println("dank");
-	}
-	
-	private static void testNetwork(){
-		Data newData = new Data(new File(Tester.path));
-		Network New = new Network(3, 3, newData);
+		assertTrue(c.checkFire()==1.0);
+		System.out.println(Perceptron.getNum());
+		System.out.println(InputNeuron.getNum());
+		assertTrue(Perceptron.getNum()==2&&InputNeuron.getNum()==0);
+		assertTrue(Neuron.getNum()==2);
+		reset();
 		
-		New.update();
-		New.getReal();
+	}
+	@Test
+	public void testData(){
+		Data test = new Data(new File(Tester.path));
+		IO io = test.nextDataSet();
+		for(int i = 0; i<io.getInputLength();i++)
+			assertTrue(io.getInputValue(i));
+	}
+	@Test
+	public void testInputNeurons(){
+//		IO simpleCase = new IO(new boolean[]{true},0);
+		InputNeuron in = new InputNeuron();
+		in.readValue(true);
+		//as the neuron has not been updated yet
+		assertTrue(in.checkFire()==0.0f);
+		in.update();
+		assertTrue(in.checkFire()==1.0f);
+		in.readValue(false);
+		assertTrue(in.checkFire()==1.0f);
+		in.update();
+		assertTrue(in.checkFire()==0.0f);
+		reset();
 	}
 	
-	private static void testInputNeurons(){
-		//shit this all got real complicated real fast
-		IO simpleCase = new IO(new boolean[]{true},0);
-		
-		Network n = new Network(1,1);
-		n.setInput(simpleCase);
-		InputNeuron I = new InputNeuron(0, n);
-		System.out.println(I.checkFire());
-		//this is false, neuron hasn't updated yet and default is false
-		I.update();
-		System.out.println(I.checkFire());
-		//the input neuron's input reads as true, so it fires after updating
-		n.setInput(new IO(new boolean[]{false} ,0));
-		I.update();
-		System.out.println(I.checkFire());
-		//after changing the input array and updating, the neuron is no longer firing
-		System.out.println("dank");
-		//fucking success with all the shallow copy bullshit this is great
-	}
-	
-	private static void testLayers(){
+	@Test
+	public void testLayerCount(){
 			Layer[] layers = new Layer[5];
 			for (int i = 0; i<layers.length;i++)
 				layers[i] = new InnerLayer();
-			System.out.println(Layer.getNum());
-			System.out.println(InnerLayer.getNum());
-			System.out.println("dank");
-		
+			assertTrue(Layer.getNum()==5);
+			assertTrue(InnerLayer.getNum()==5);
+			reset();
+	}
+	@Test
+	public void testIO(){
+		IO test = new IO( new boolean[]{true,true,false}, new boolean[]{false,true,true} );
+			assertTrue(test.getInputValue(0));
+			assertTrue(test.getInputValue(1));
+			assertTrue(!test.getInputValue(2));
+			
+			assertTrue(!test.getOutputValue(0));
+			assertTrue(test.getOutputValue(1));
+			assertTrue(test.getOutputValue(2));
 	}
 	
-	private static void testIO(){
-		IO test = new IO(new boolean[]{true,true,false},new boolean[]{false,true,true});
-		System.out.println("inputs: ");
-		for(int i = 0; i < 3; i++)
-			System.out.println(test.getInputValue(i));
-		System.out.println("outputs: ");
-		for(int i = 0; i < 3; i++)
-			System.out.println(test.getOutputValue(i));
-		System.out.println("finished IO testing");
+	@Test
+	public void testNetwork(){
+		Data newData = new Data(new File(Tester.path));
+		Network New = new Network(1, 3, newData);
+		New.run();
+		for (boolean out : New.getOutput())
+			assertTrue(out);
+		reset();
 	}
 	
-	private static void testData(){
-		
-		Data test = new Data(new File(Tester.path));
-		IO io = test.nextDataSet();
-		for(int i = 0; i<2;i++)
-			System.out.println(io.getInputValue(i));
-	}
-	private static void testAll(){
-//		  System.out.println("Testing full functionality: ");
-//		  Neuron[] raw;
-//		  Data ANDFunctionTestData= new Data(new String(datatext.txt));
-//		  raw = new Neuron[3];
-//		  for(int i = 0; i<raw.length;i++)
-//		  	raw[i] = new InputNeuron(i);
-//		  InputLayer in = new InputLayer(raw);
-//		  in.hookToDataset(ANDFunctionTestData.nextDataset());
-	}
-	public static void main(String[] args) {
-		setPath("/Users/sparr/workspace/neuralnetwork/src/data/datasets.txt");
-		testPerceptrons();
-		testInputNeurons();
-		testLayers();
-		testIO();
-		testData();
-		testNetwork();
-	}
-
 }
